6b4ed0950d06121df46082306439d907f82219be
2357or <61523777+2357or@users.noreply.github.com>
2021-02-23 21:23:12 +0900

    feat: NumberExtension

--- R2357/R2357/Extensions/NumberExtension.cs
+++ R2357/R2357/Extensions/NumberExtension.cs
@@ -1,9 +1,52 @@
-﻿namespace R2357.Extensions {
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace R2357.Extensions {
 
     /// <summary>
     /// Extension methods (Number).
     /// </summary>
     public static class NumberExtension {
+
+        /// <summary>
+        /// [ExtensionMethod] Returns the value of ex power.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Pow(this float f, float ex) {
+            return Mathf.Pow(f, ex);
+        }
+
+        /// <summary>
+        /// [ExtensionMethod] Returns the value of square root.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Sqrt(this float f) {
+            return Mathf.Sqrt(f);
+        }
+
+        /// <summary>
+        /// [ExtensionMethod] Returns the value rounded to the nearest integer.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Round(this float f) {
+            return Mathf.Round(f);
+        }
+
+        /// <summary>
+        /// [ExtensionMethod] Returns the smallest integer greater than or equal to this value.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Ceil(this float f) {
+            return Mathf.Ceil(f);
+        }
+
+        /// <summary>
+        /// [ExtensionMethod] Returns the smallest integer that is less than or equal to this value.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Floor(this float f) {
+            return Mathf.Floor(f);
+        }
 
     }
 }
